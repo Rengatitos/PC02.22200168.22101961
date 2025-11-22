e515a8c7775d4ce09ef4169c693128de6df28b2f
Camila <22200168@ue.edu.pe>
2025-11-21 20:03:05 -0500

    LoginForm

---
+++ pc02DAW/src/pages/DigimonPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>    Esta es la pagina de la API Digimon </h1>
+
+</template>
+
--- pc02DAW/src/router/routes.js
+++ pc02DAW/src/router/routes.js
@@ -4,6 +4,15 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  {
+    path: '/login',
+    component: () => import('src/components/auth/LoginForm.vue'),
+  },
+  {
+  path: '/digimon',
+  component: () => import('src/pages/DigimonPage.vue')
+},
+
 
   // Always leave this as last one,
   // but you can also remove it
